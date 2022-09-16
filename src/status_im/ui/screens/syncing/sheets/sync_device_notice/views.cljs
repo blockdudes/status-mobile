5f8dd87e3024dcc1950113d85cb707ae436c6046
siddarthkay <siddarthkay@gmail.com>
2022-09-16 15:31:56 +0530

    password sheet and scan QR before login

--- src/status_im/ui/screens/syncing/sheets/sync_device_notice/views.cljs
+++ src/status_im/ui/screens/syncing/sheets/sync_device_notice/views.cljs
@@ -4,6 +4,7 @@
             [status-im.ui.screens.syncing.sheets.sync-device-notice.styles :as styles]
             [status-im.ui.screens.syncing.sheets.sync-generated-code.views :as sync-generated-code]
             [status-im.ui.screens.syncing.sheets.scan-code.views :as scan-code]
+            [status-im.ui.screens.syncing.sheets.enter-password.views :as enter-password]
             [quo.core :as quo]
             [quo.design-system.colors :as colors]
             [re-frame.core :as re-frame]
@@ -14,7 +15,6 @@
             [status-im.utils.utils :as utils]
             [quo2.components.buttons.button :as quo2-button]
             [status-im.utils.handlers :refer [>evt]]
-            [status-im.qr-scanner.core :as qr-scanner]
             [status-im.react-native.resources :as resources]))
 
 (defn hide-sheet-and-dispatch [event]
@@ -29,7 +29,7 @@
    [rn/view {:style styles/sync-devices-body-container}
     [rn/text {:style styles/header-text} "Sync a new device"]
     [rn/text {:style styles/instructions-text} "You own your data. Syncronize it amoung all your devices."]
-    [rn/text {:style styles/list-item-text} "1.  Verify login with password or KeyCard"]
+    [rn/text {:style styles/list-item-text} "1.  Verify login with password"]
     [rn/text {:style styles/list-item-text} "2.  Reveal a temporary QR and Sync Code"]
     [rn/text {:style styles/list-item-text} "3.  Share that info with your new device"]
     [quo2-button/button {:type   :secondary
@@ -39,28 +39,30 @@
                          :on-press #(>evt [:bottom-sheet/show-sheet
                                            {:showHandle? false
                                             :content (fn []
-                                                       [sync-generated-code/views])}])}
+                                                       [enter-password/views])}])}
      "Setup Syncing"]
+;
+;    [rn/view {:style styles/secondary-body-container}
+;     [rn/text {:style styles/header-text} "Have a Sync Code?"]
+;
+;     [quo2-button/button {:type   :secondary
+;                          :size     40
+;                          :style    styles/setup-syncing-button
+;                          :before   :main-icons2/face-id20
+;                          :on-press #(utils/show-confirmation {:title "We don't do that here"
+;                                                               :confirm-button-text "Okay fineee"
+;                                                               :cancel-button-text "but?"})}
+;      "Enter Sync Code"]
+;
+;     [rn/text {:style styles/instructions-text} "OR"]
+;     [quo2-button/button {:type   :secondary
+;                          :size     40
+;                          :style    styles/setup-syncing-button
+;                          :before   :main-icons2/face-id20
+;                          :on-press #(do
+;                                       (re-frame/dispatch [:bottom-sheet/hide])
+;                                       (re-frame/dispatch [::qr-scanner/scan-code
+;                                                           {:handler ::qr-scanner/on-scan-success}]))}
+;      "Scan Sync Code"]]
 
-    [rn/view {:style styles/secondary-body-container}
-     [rn/text {:style styles/header-text} "Have a Sync Code?"]
-
-     [quo2-button/button {:type   :secondary
-                          :size     40
-                          :style    styles/setup-syncing-button
-                          :before   :main-icons2/face-id20
-                          :on-press #(utils/show-confirmation {:title "We don't do that here"
-                                                               :confirm-button-text "Okay fineee"
-                                                               :cancel-button-text "but?"})}
-      "Enter Sync Code"]
-
-     [rn/text {:style styles/instructions-text} "OR"]
-     [quo2-button/button {:type   :secondary
-                          :size     40
-                          :style    styles/setup-syncing-button
-                          :before   :main-icons2/face-id20
-                          :on-press #(do
-                                       (re-frame/dispatch [:bottom-sheet/hide])
-                                       (re-frame/dispatch [::qr-scanner/scan-code
-                                                           {:handler ::qr-scanner/on-scan-success}]))}
-      "Scan Sync Code"]]]])
+    ]])
